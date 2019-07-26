bcc13c2fa12e70a25f0ca1eb0360731552289bf1
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2019-07-26 01:03:13 -0400

    Reasonably working MVP of personal website Bio and links to some socials Is not mobile friendly ATM

---
+++ src/components/LinkContainer.js
@@ -0,0 +1,58 @@
+import "../styles/homepage.scss"
+import React from "react"
+import resumePdf from "../assets/lucas_jenkins_resume.pdf"
+import linkedinIcon from "../assets/linkedin_icon.png"
+import resumeIcon from "../assets/resume_icon.png"
+import githubIcon from "../assets/github_icon.png"
+import instaIcon from "../assets/insta_icon.png"
+import spotifyIcon from "../assets/spotify_icon.png"
+
+
+const LinkContainer = (props) => {
+    return(
+    <div className="linkContainer">
+        <div className="linkItem">
+            <p className="linkDescription">
+                For my most professional side
+            </p>
+            <a href="https://www.linkedin.com/in/lucas-jenkins/" target="_blank"> 
+                <img src={linkedinIcon} alt="LinkedIn icon link"/> 
+            </a>
+        </div>
+        <div className="linkItem">
+            <p className="linkDescription">
+                For my LinkedIn info in PDF form
+            </p>
+            <a href={resumePdf} download> 
+                <img src={resumeIcon} alt="Resume icon link"/> 
+            </a>
+        </div>
+        <div className="linkItem">
+            <p className="linkDescription">
+                To look at some code I've written
+            </p>
+            <a href="https://github.com/jenklu" target="_blank"> 
+                <img src={githubIcon} alt="Github icon link"/> 
+            </a>
+        </div>
+        <div className="linkItem">
+            <p className="linkDescription">
+                For some pics of my friends and me
+            </p>
+            <a href="https://www.instagram.com/leroy.j12/" target="_blank"> 
+                <img src={instaIcon} alt="Instagram icon link"/> 
+            </a>
+        </div>
+        <div className="linkItem">
+            <p className="linkDescription">
+                To see what music I’m into
+            </p>
+            <a href="https://open.spotify.com/user/1231801536?si=s6Oi3haTQH6U88Er0Bqt8A" target="_blank">
+                <img src={spotifyIcon} alt="Spotify icon link"/>
+            </a>
+        </div>
+    </div>
+    );
+}
+
+export default LinkContainer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,39 @@
 import React from "react"
-import("../styles/homepage.scss")
+import "../styles/homepage.scss"
+import LinkContainer from "../components/LinkContainer.js"
+
 class Index extends React.Component{
     render() {
         return (
-            <div>
-            lucas jenkins' personal website<br />
-            WORK IN PROGRESS
+            <div className="mainContainer">
+                <div id="welcomeHeader">
+                    <p className="headerText">
+                    Welcome to Lucas Jenkins’ homepage!
+                    </p>
+                </div>
+                <div className="separationHeader">
+                    <p className="headerText">
+                    About Me
+                    </p>
+                </div>
+                <div>
+                    <p className="aboutText">
+                    I’m Lucas, a 22-year old who’s about to move to San Francisco. 
+                    I was born and raised in Boston, went to LA for college, and loved 
+                    California so much I decided to stay out there for a full-time job 
+                    as a software engineer at Google. Besides writing code and spending 
+                    ime with friends/family, I enjoy playing basketball, playing the 
+                    guitar, and looking for the best taco places in any city I visit. 
+                    Check out the rest of my website to learn more about me and what 
+                    I’ve worked on.
+                    </p>
+                </div>
+                <div className="separationHeader">
+                    <p className="headerText">
+                    Outside Links
+                    </p>
+                </div>
+                <LinkContainer />
             </div>
         ) 
     }
